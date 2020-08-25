2f4957f6cbfe73ecadedb70b982f0a66ee417edf
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 23:22:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,10 +8,11 @@ def find_even_values(src)
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
-      
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
       element_index += 1
     end
     row_index += 1
   end
-
 end
